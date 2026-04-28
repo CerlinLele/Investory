d74d2686b64fd2136725644f8a42e44cdc333a1f
Cerlin <yilindeng1@gmail.com>
2026-04-28 23:15:13 +1000

    feat(agent-core): add task result contracts

---
+++ src/investory/agent_core/contracts/__init__.py
@@ -0,0 +1,17 @@
+from investory.agent_core.contracts.result_types import (
+    TaskError,
+    TaskErrorType,
+    TaskResult,
+    TaskStage,
+    normalize_task_error,
+)
+from investory.agent_core.contracts.task_spec import TaskSpec
+
+__all__ = [
+    "TaskError",
+    "TaskErrorType",
+    "TaskResult",
+    "TaskSpec",
+    "TaskStage",
+    "normalize_task_error",
+]
