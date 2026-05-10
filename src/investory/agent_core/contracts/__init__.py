c703a6258cd4ed195b861adff29edcaa2b3dd9c9
Cerlin <yilindeng1@gmail.com>
2026-05-10 18:05:15 +1000

    feat(agent-core): add action contract schemas

--- src/investory/agent_core/contracts/__init__.py
+++ src/investory/agent_core/contracts/__init__.py
@@ -1,5 +1,11 @@
-from investory.agent_core.contracts.action_decision import (
+from investory.agent_core.contracts.action_contract import (
+    ActionCall,
     ActionName,
+    ActionResult,
+    ActionStatus,
+    TaskDecision,
+)
+from investory.agent_core.contracts.action_decision import (
     AskMissingFieldsAction,
     build_ask_missing_fields_action,
     decide_missing_fields_action,
@@ -16,7 +22,11 @@ from investory.agent_core.contracts.task_spec import TaskSpec
 
 __all__ = [
     "ActionName",
+    "ActionCall",
+    "ActionResult",
+    "ActionStatus",
     "AskMissingFieldsAction",
+    "TaskDecision",
     "TaskError",
     "TaskErrorType",
     "TaskFlowState",
